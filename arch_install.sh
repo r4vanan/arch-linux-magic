57d78c59d2be156eea5c030846e71ac0d7564dba
Vinoth Kumar <35825039+r4vanan@users.noreply.github.com>
2022-06-05 10:35:27 +0530

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -61,7 +61,7 @@ pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xback
      fzf man-db xwallpaper python-pywal unclutter xclip maim \
      zip unzip unrar p7zip xdotool papirus-icon-theme brightnessctl  \
      dosfstools ntfs-3g git sxhkd zsh pipewire pipewire-pulse \
-     emacs-nox arc-gtk-theme rsync qutebrowser dash \
+     networkmanager arc-gtk-theme rsync qutebrowser dash \
      xcompmgr libnotify dunst slock jq aria2 cowsay \
      dhcpcd connman wpa_supplicant rsync pamixer mpd ncmpcpp \
      zsh-syntax-highlighting xdg-user-dirs libconfig \
@@ -90,20 +90,16 @@ git clone --separate-git-dir=$HOME/.dotfiles https://github.com/bugswriter/dotfi
 rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
 rm -r tmpdotfiles
 # dwm: Window Manager
-git clone --depth=1 https://github.com/Bugswriter/dwm.git ~/.local/src/dwm
+git clone --depth=1 https://github.com/r4vanan/dwm.git ~/.local/src/dwm
 sudo make -C ~/.local/src/dwm install
 
 # st: Terminal
-git clone --depth=1 https://github.com/Bugswriter/st.git ~/.local/src/st
+git clone --depth=1 https://github.com/r4vanan/st.git ~/.local/src/st
 sudo make -C ~/.local/src/st install
 
 # dmenu: Program Menu
 git clone --depth=1 https://github.com/Bugswriter/dmenu.git ~/.local/src/dmenu
 sudo make -C ~/.local/src/dmenu install
-
-# dmenu: Dmenu based Password Prompt
-git clone --depth=1 https://github.com/ritze/pinentry-dmenu.git ~/.local/src/pinentry-dmenu
-sudo make -C ~/.local/src/pinentry-dmenu clean install
 
 # dwmblocks: Status bar for dwm
 git clone --depth=1 https://github.com/bugswriter/dwmblocks.git ~/.local/src/dwmblocks
@@ -119,6 +115,8 @@ mkdir dl dox imp music pix pub code
 
 ln -s ~/.config/x11/xinitrc .xinitrc
 ln -s ~/.config/shell/profile .zprofile
+# remaping capslock and escape keys
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mv ~/.oh-my-zsh ~/.config/zsh/oh-my-zsh
 rm ~/.zshrc ~/.zsh_history
